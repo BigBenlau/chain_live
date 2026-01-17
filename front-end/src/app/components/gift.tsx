66f8aca476a0b7cdbd3f7f1f3ad544f370a73775
Ace You <304076267@qq.com>
2026-01-17 16:49:10 +0800

    添加转帐方法

--- front-end/src/app/components/gift.tsx
+++ front-end/src/app/components/gift.tsx
@@ -1,39 +1,53 @@
+import { parseEther } from "viem";
+import { useSendTransaction } from 'wagmi'
+
 function Gift() {
 
   const datas = [
     {
       id: 1,
       url: "/gift1.png",
-      price: 1,
+      price: "0.1",
     },
     {
       id: 2,
       url: "/gift2.png",
-      price: 5,
+      price: "0.2",
     },
     {
       id: 3,
       url: "/gift3.png",
-      price: 20,
+      price: "0.3",
     }
   ];
 
+  const { data: hash, sendTransaction } = useSendTransaction()
+
   function OnItemClick(id: number) {
     console.log(id);
+
+    const data = datas.find(data => data.id === id);
+    if (!data) return;
+
+    sendTransaction({
+      to: '0x435a345bB8eC10b30738217332216849506aCCcF',
+      value: parseEther(data.price),
+    })
   }
 
   return (
-    <div className="mt-6">
-      <div className="flex overflow-x-auto gap-4">
+    <div className="mt-6 flex flex-col items-center justify-center">
+      <div className="flex justify-center gap-4">
         {
           datas.map(data => <GiftItem key={data.id} url={data.url} OnItemClick={OnItemClick} id={data.id} price={data.price} />)
         }
       </div>
+      {hash && <div>Transaction Hash: {hash}</div>}
     </div>
   )
 }
 
-function GiftItem(props: { id: number, url: string, OnItemClick: (id: number) => void, price: number }) {
+function GiftItem(props: { id: number, url: string, OnItemClick: (id: number) => void, price: string }) {
   return (
     <div className="text-center flex-shrink-0">
       <div>
